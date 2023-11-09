24440beb68b78adf3970c1a794f6948eaa2dc8f8
markCho <markkcho@outlook.com>
2023-11-09 19:33:31 +1100

    back updateCartQ function to amazon.js

--- data/cart.js
+++ data/cart.js
@@ -11,22 +11,10 @@ export function addToCart(productId) {
 
   if (matchingItem) {
     matchingItem.quantity += 1;
-  } else {
-    cart.push({
+    } else {
+      cart.push({
       productId: productId,
       quantity: 1
-    });
-  }
-};
-
-
-export function updateCartQuantity() {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
-  document.querySelector('.js-cart-quantity')
-    .innerHTML = cartQuantity;
+      });
+    };
 };
